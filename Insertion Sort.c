9cf3a3e4e855efef202077215f827a61d0562e3e
Md Mahfuzur Rahman <63856744+mahfuzur-mafu@users.noreply.github.com>
2021-02-03 20:55:52 +0600

    Add files via upload

---
+++ Insertion Sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+
+    int n,temp;
+    int arr[100];
+    printf("Elements :");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Elements no [%d]: ",i+1);
+        scanf("%d",&arr[i]);;
+    }
+    for (int i = 1 ; i <= n - 1; i++)
+    {
+        int j;
+        j = i;
+        while ( j > 0 && arr[j-1] > arr[j])
+        {
+            temp     = arr[j];
+            arr[j]   = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+    }
+    printf("After Sorted : ");
+    for (int i = 0; i <= n - 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
